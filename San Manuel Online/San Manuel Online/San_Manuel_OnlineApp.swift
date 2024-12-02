8ce215dde3a691633ef1416d03de9c62e035dafc
Dias <dias.atudinov@gmail.com>
2024-12-02 20:29:40 +0500

    first commit

---
+++ San Manuel Online/San Manuel Online/San_Manuel_OnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  San_Manuel_OnlineApp.swift
+//  San Manuel Online
+//
+//  Created by Dias Atudinov on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct San_Manuel_OnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
