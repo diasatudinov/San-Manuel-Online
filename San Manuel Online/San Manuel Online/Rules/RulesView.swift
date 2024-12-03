08ac114e115e6fd29b995019aed701d457d2c037
Dias <dias.atudinov@gmail.com>
2024-12-03 14:37:51 +0500

    Rules View

--- San Manuel Online/San Manuel Online/Rules/RulesView.swift
+++ San Manuel Online/San Manuel Online/Rules/RulesView.swift
@@ -8,8 +8,54 @@
 import SwiftUI
 
 struct RulesView: View {
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            VStack {
+                ZStack {
+                    HStack {
+                        Spacer()
+                        TextWithBorder(text: "Rules ", font: .custom(Fonts.mazzardM.rawValue, size: 55), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 2)
+                            .textCase(.uppercase)
+                        
+                        Spacer()
+                    }
+                    HStack {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            ZStack {
+                                Image(.backBtn)
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                                
+                                
+                            }.frame(height: 65)
+                            
+                        }
+                        Spacer()
+                    }.padding()
+                }
+                
+                
+                TextWithBorder(text: "The player starts with 6 amulets of different colors, which are located at the bottom of the screen (inventory). The player pulls the selected amulet onto an empty cell. After placing the amulet, it disappears, and a new amulet appears in the inventory.\nOpponent's Actions (AI): The opponent chooses amulets of the same type as the player, but they are marked with a different color. His goal is to block strategically important player moves or try to collect the most points. ", font: .custom(Fonts.mazzardM.rawValue, size: 26), textColor: .mainBrown, borderColor: .mainYellow, borderWidth: 2)
+                    .textCase(.uppercase)
+                    .multilineTextAlignment(.center)
+                    .minimumScaleFactor(0.5)
+                
+                
+                Spacer()
+            }
+        }
+        .background(
+            Image(.background)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .scaledToFill()
+            
+        )
     }
 }
 
