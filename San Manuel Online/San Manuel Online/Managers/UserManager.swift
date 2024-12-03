48969741dcd32ddc4096872f6ad322ac84042585
Dias <dias.atudinov@gmail.com>
2024-12-03 14:37:37 +0500

    User manager

--- San Manuel Online/San Manuel Online/Managers/UserManager.swift
+++ San Manuel Online/San Manuel Online/Managers/UserManager.swift
@@ -5,4 +5,43 @@
 //  Created by Dias Atudinov on 03.12.2024.
 //
 
-import Foundation
+import SwiftUI
+
+class User: ObservableObject {
+    static let shared = User()
+    
+    @AppStorage("coins") var storedCoins: Int = 100
+    @Published var coins: Int = 10
+    
+    @AppStorage("game1Level") var storedLevel: Int = 1
+    @Published var level: Int = 1
+    
+    @AppStorage("game2Level") var storedLevelGame: Int = 1
+    @Published var levelGame: Int = 1
+    
+    init() {
+        coins = storedCoins
+        level = storedLevel
+        levelGame = storedLevelGame
+    }
+    
+    func updateUserCoins(for coins: Int) {
+        self.coins += coins
+        storedCoins = self.coins
+    }
+    
+    func minusUserCoins(for coins: Int) {
+        self.coins -= coins
+        storedCoins = self.coins
+    }
+    
+    func updateUserLevel() {
+        self.level += 1
+        storedLevel = self.level
+    }
+    
+    func updateUserLevelGame2() {
+        self.levelGame += 1
+        storedLevelGame = self.levelGame
+    }
+}
