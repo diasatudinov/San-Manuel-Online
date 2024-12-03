9024458e61c96f3c4e627adffd68afbe116eedc6
Dias <dias.atudinov@gmail.com>
2024-12-03 14:38:05 +0500

    Splash Screen View

--- San Manuel Online/San Manuel Online/Menu/SplashScreen.swift
+++ San Manuel Online/San Manuel Online/Menu/SplashScreen.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @State private var scale: CGFloat = 1.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            
+            VStack {
+                Spacer()
+                TextWithBorder(text: "LOADING...", font: .custom(Fonts.mazzardM.rawValue, size: 60), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 2)
+                Spacer()
+            }
+        }.background(
+            Image(.background)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .scaledToFill()
+            
+        )
     }
 }
 
 #Preview {
     SplashScreen()
 }
+
+struct TextWithBorder: View {
+    let text: String
+    let font: Font
+    let textColor: Color
+    let borderColor: Color
+    let borderWidth: CGFloat
+
+    var body: some View {
+        ZStack {
+            // Multiple layers of text for the border effect
+            ForEach([1, 1], id: \.self) { xOffset in
+                ForEach([1, 1], id: \.self) { yOffset in
+                    Text(text)
+                        .font(font)
+                        .foregroundColor(borderColor)
+                        .offset(x: CGFloat(xOffset) * borderWidth, y: CGFloat(yOffset) * borderWidth)
+                }
+            }
+            .offset(x: 1, y: 1)
+            // Main text layer
+            Text(text)
+                .font(font)
+                .foregroundColor(textColor)
+        }
+    }
+}
+   
+    
+//.offset(x: -2, y: 2)
+
