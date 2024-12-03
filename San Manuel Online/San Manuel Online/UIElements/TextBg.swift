f101be21198351ce5b3650ab57776737b3316f6d
Dias <dias.atudinov@gmail.com>
2024-12-03 14:37:29 +0500

    Text background ui element

--- San Manuel Online/San Manuel Online/UIElements/TextBg.swift
+++ San Manuel Online/San Manuel Online/UIElements/TextBg.swift
@@ -7,12 +7,37 @@
 
 import SwiftUI
 
+enum TextBgType {
+    case black, gold
+}
+
 struct TextBg: View {
+    var type: TextBgType = .gold
+    var height: CGFloat
+    var text: String
+    var textSize: CGFloat
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if type == .black {
+                Image(.textBlackBg)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: height)
+            } else {
+                Image(.textGoldBg)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: height)
+            }
+            
+            TextWithBorder(text: text, font: .custom(Fonts.mazzardM.rawValue, size: textSize), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 3)
+                .textCase(.uppercase)
+            
+                
+        }
     }
 }
 
 #Preview {
-    TextBg()
+    TextBg(height: 100, text: "Loading...", textSize: 32)
 }
