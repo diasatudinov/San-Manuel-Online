3a098349e4344b02559dde9545faf71572aa23f9
Dias <dias.atudinov@gmail.com>
2024-12-04 20:44:48 +0500

    Menu view

--- San Manuel Online/San Manuel Online/Managers/UserManager.swift
+++ San Manuel Online/San Manuel Online/Managers/UserManager.swift
@@ -13,16 +13,8 @@ class User: ObservableObject {
     @AppStorage("coins") var storedCoins: Int = 100
     @Published var coins: Int = 10
     
-    @AppStorage("game1Level") var storedLevel: Int = 1
-    @Published var level: Int = 1
-    
-    @AppStorage("game2Level") var storedLevelGame: Int = 1
-    @Published var levelGame: Int = 1
-    
     init() {
         coins = storedCoins
-        level = storedLevel
-        levelGame = storedLevelGame
     }
     
     func updateUserCoins(for coins: Int) {
@@ -33,15 +25,5 @@ class User: ObservableObject {
     func minusUserCoins(for coins: Int) {
         self.coins -= coins
         storedCoins = self.coins
-    }
-    
-    func updateUserLevel() {
-        self.level += 1
-        storedLevel = self.level
-    }
-    
-    func updateUserLevelGame2() {
-        self.levelGame += 1
-        storedLevelGame = self.levelGame
     }
 }
--- San Manuel Online/San Manuel Online/Menu/MenuView.swift
+++ San Manuel Online/San Manuel Online/Menu/MenuView.swift
@@ -17,10 +17,15 @@ struct MenuView: View {
     @State private var showHowToPlay = false
     
     @StateObject var user = User.shared
-    //@StateObject var achievementsVM = AchievementsViewModel()
+    @StateObject var achievementsVM = AchievementsViewModel()
     //    @StateObject var leaderboardVM = LeaderboardViewModel()
-//      @StateObject var settingsVM = SettingsModel()
+      @StateObject var settingsVM = SettingsModel()
 //    @StateObject var collectionVM = CollectionViewModel()
+    
+    
+    @State private var timeRemaining: String = "24:00"
+    @State private var timerActive: Bool = false
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         GeometryReader { geometry in
@@ -86,7 +91,13 @@ struct MenuView: View {
                                 Button {
                                     showDailyBonus = true
                                 } label: {
+                                    
+                                    ZStack {
                                     TextBg(height: 75, text: "Daily Bonus", textSize: 35)
+                                        TextWithBorder(text: "\(timeRemaining)", font: .custom(Fonts.mazzardM.rawValue, size: 16), textColor: .mainBrown, borderColor: .mainYellow, borderWidth: 1)
+                                            .offset(x: 95, y: -15)
+                                            
+                                    }
                                 }
                                 
                                 Button {
@@ -104,6 +115,7 @@ struct MenuView: View {
                         
                         VStack {
                             HStack(spacing: 5){
+                                Spacer()
                                 ZStack {
                                     Image(.coinBg)
                                         .resizable()
@@ -122,7 +134,7 @@ struct MenuView: View {
                                             
                                     }
                                 }
-                                
+                                Spacer()
                             }.padding([.top, .trailing], 20)
                             Spacer()
                         }
@@ -136,7 +148,7 @@ struct MenuView: View {
                                     Button {
                                         showPlay = true
                                     } label: {
-                                        TextBg(height: 65, text: "Play", textSize: 35)
+                                        TextBg(height: 65, text: "Play", textSize: 32)
                                     }
                                 }
                                 HStack(spacing: 15) {
@@ -144,13 +156,13 @@ struct MenuView: View {
                                     Button {
                                         showTraining = true
                                     } label: {
-                                        TextBg(height: 65, text: "Training", textSize: 35)
+                                        TextBg(height: 65, text: "Training", textSize: 32)
                                     }
                                     
                                     Button {
                                         showSettings = true
                                     } label: {
-                                        TextBg(height: 65, text: "Settings", textSize: 35)
+                                        TextBg(height: 65, text: "Settings", textSize: 32)
                                     }
                                     Spacer()
                                 }
@@ -166,7 +178,12 @@ struct MenuView: View {
                                     Button {
                                         showDailyBonus = true
                                     } label: {
-                                        TextBg(height: 65, text: "Daily Bonus", textSize: 35)
+                                        ZStack {
+                                            TextBg(height: 65, text: "Daily Bonus", textSize: 27)
+                                            TextWithBorder(text: "\(timeRemaining)", font: .custom(Fonts.mazzardM.rawValue, size: 16), textColor: .mainBrown, borderColor: .mainYellow, borderWidth: 1)
+                                                .offset(x: 75, y: -15)
+                                                
+                                        }
                                     }
                                     Spacer()
                                 }
@@ -175,7 +192,7 @@ struct MenuView: View {
                                     Button {
                                         showHowToPlay = true
                                     } label: {
-                                        TextBg(height: 65, text: "How to play", textSize: 30)
+                                        TextBg(height: 65, text: "How to play", textSize: 27)
                                     }
                                     Spacer()
                                 }
@@ -194,6 +211,12 @@ struct MenuView: View {
                     .scaledToFill()
                 
             )
+            .onAppear {
+                updateTimer()
+            }
+            .onReceive(timer) { _ in
+                updateTimer()
+            }
 //            .onAppear {
 //                if settingsVM.musicEnabled {
 //                    MusicPlayer.shared.playBackgroundMusic()
@@ -207,24 +230,47 @@ struct MenuView: View {
 //                }
 //            }
             .fullScreenCover(isPresented: $showPlay) {
-               // TrainGameView(settingsVM: settingsVM)
+                SettingsView(settings: settingsVM)
             }
             .fullScreenCover(isPresented: $showTraining) {
-              //  SequenceGameView(settingsVM: settingsVM)
+                SettingsView(settings: settingsVM)
             }
             .fullScreenCover(isPresented: $showSettings) {
-               // GetCardView(collectionVM: collectionVM)
+                SettingsView(settings: settingsVM)
             }
             .fullScreenCover(isPresented: $showAchievements) {
-               // CollectionView(collectionVM: collectionVM)
+                AchievementsView(viewModel: achievementsVM)
             }
             .fullScreenCover(isPresented: $showDailyBonus) {
-               // SettingsView(settings: settingsVM)
+                DailyBonusView()
             }
             .fullScreenCover(isPresented: $showHowToPlay) {
-               // SettingsView(settings: settingsVM)
+                RulesView()
             }
             
+        }
+    }
+    
+    private func updateTimer() {
+        guard let lastPressDate = UserDefaults.standard.object(forKey: "LastPressDate") as? Date else {
+            timeRemaining = "00:00" // If no saved date, assume timer is full
+            timerActive = false
+            return
+        }
+        
+        let now = Date()
+        let totalDuration: TimeInterval = 24 * 60 * 60 // 24 hours in seconds
+        let elapsedTime = now.timeIntervalSince(lastPressDate) // Time since lastPressDate
+        let remainingTime = totalDuration - elapsedTime // Time left
+        
+        if remainingTime <= 0 {
+            timeRemaining = "00:00"
+            timerActive = false
+        } else {
+            timerActive = true
+            let hours = Int(remainingTime) / 3600
+            let minutes = (Int(remainingTime) % 3600) / 60
+            timeRemaining = String(format: "%02d:%02d", hours, minutes) // Format as hh:mm
         }
     }
 }
