65dd546deb12358e7d26548e83b4d84271e38d21
Dias <dias.atudinov@gmail.com>
2024-12-04 20:44:58 +0500

    Settings view

--- San Manuel Online/San Manuel Online/Settings/SettingsView.swift
+++ San Manuel Online/San Manuel Online/Settings/SettingsView.swift
@@ -8,11 +8,107 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var settings: SettingsModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            VStack {
+                ZStack {
+                    HStack {
+                        Spacer()
+                        TextWithBorder(text: "Settings", font: .custom(Fonts.mazzardM.rawValue, size: 55), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 2)
+                            .textCase(.uppercase)
+                        
+                        Spacer()
+                    }
+                    HStack {
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            ZStack {
+                                Image(.backBtn)
+                                    .resizable()
+                                    .scaledToFit()
+                                
+                            }.frame(height: 65)
+                            
+                        }
+                        Spacer()
+                    }.padding()
+                }
+                
+                VStack(spacing: 30) {
+                    ZStack {
+                        
+                        Image(.settingsBg)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 80)
+                        
+                        HStack(spacing: 16) {
+                            TextWithBorder(text: "SOUND", font: .custom(Fonts.mazzardM.rawValue, size: 30), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 2)
+                                .textCase(.uppercase)
+                            Button {
+                                settings.soundEnabled.toggle()
+                            } label: {
+                                if settings.soundEnabled {
+                                    Image(.on)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 30)
+                                } else {
+                                    Image(.off)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 30)
+                                }
+                            }
+                            
+                        }
+                    }
+                   
+                    ZStack {
+                        
+                        Image(.settingsBg)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 80)
+                        HStack(spacing: 16) {
+                            TextWithBorder(text: "MUSIC", font: .custom(Fonts.mazzardM.rawValue, size: 30), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 2)
+                                .textCase(.uppercase)
+                            Button {
+                                settings.musicEnabled.toggle()
+                            } label: {
+                                if settings.musicEnabled {
+                                    Image(.on)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 30)
+                                } else {
+                                    Image(.off)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 30)
+                                }
+                            }
+                        }
+                    }.padding(.bottom, 50)
+                   
+                }
+                
+                Spacer()
+            }
+        }.background(
+            Image(.background)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .scaledToFill()
+            
+        )
     }
 }
 
 #Preview {
-    SettingsView()
+    SettingsView(settings: SettingsModel())
 }
