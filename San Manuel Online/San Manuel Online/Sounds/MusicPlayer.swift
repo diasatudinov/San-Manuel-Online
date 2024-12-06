ac3bfa4cf81cb4175a451b4072482bf62094dfec
Dias <dias.atudinov@gmail.com>
2024-12-06 17:15:13 +0500

    Music player

--- San Manuel Online/San Manuel Online/Sounds/MusicPlayer.swift
+++ San Manuel Online/San Manuel Online/Sounds/MusicPlayer.swift
@@ -1,8 +1,22 @@
-//
-//  MusicPlayer.swift
-//  San Manuel Online
-//
-//  Created by Dias Atudinov on 06.12.2024.
-//
+import AVFoundation
 
-import Foundation
+class MusicPlayer {
+    static let shared = MusicPlayer()
+    var audioPlayer: AVAudioPlayer?
+
+    func playBackgroundMusic() {
+        guard let url = Bundle.main.url(forResource: "backgroundSMO", withExtension: "mp3") else { return }
+
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.numberOfLoops = -1 // Infinite loop
+            audioPlayer?.play()
+        } catch {
+            print("Could not play background music: \(error)")
+        }
+    }
+
+    func stopBackgroundMusic() {
+        audioPlayer?.stop()
+    }
+}
