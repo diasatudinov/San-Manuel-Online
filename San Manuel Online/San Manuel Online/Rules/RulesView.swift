f380c56c982301eee0cca4c2e023131a4d1d0c69
Dias <dias.atudinov@gmail.com>
2024-12-06 17:14:50 +0500

    Rules view

--- San Manuel Online/San Manuel Online/Rules/RulesView.swift
+++ San Manuel Online/San Manuel Online/Rules/RulesView.swift
@@ -11,16 +11,16 @@ struct RulesView: View {
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
         ZStack {
-            
+            WV(url: URL(string: Links.ruleURL)!)
             VStack {
                 ZStack {
-                    HStack {
-                        Spacer()
-                        TextWithBorder(text: "Rules ", font: .custom(Fonts.mazzardM.rawValue, size: 55), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 2)
-                            .textCase(.uppercase)
-                        
-                        Spacer()
-                    }
+//                    HStack {
+//                        Spacer()
+//                        TextWithBorder(text: "Rules ", font: .custom(Fonts.mazzardM.rawValue, size: 55), textColor: .mainYellow, borderColor: .mainBrown, borderWidth: 2)
+//                            .textCase(.uppercase)
+//                        
+//                        Spacer()
+//                    }
                     HStack {
                         Button {
                             presentationMode.wrappedValue.dismiss()
@@ -38,13 +38,13 @@ struct RulesView: View {
                         Spacer()
                     }.padding()
                 }
-                
-                
-                TextWithBorder(text: "The player starts with 6 amulets of different colors, which are located at the bottom of the screen (inventory). The player pulls the selected amulet onto an empty cell. After placing the amulet, it disappears, and a new amulet appears in the inventory.\nOpponent's Actions (AI): The opponent chooses amulets of the same type as the player, but they are marked with a different color. His goal is to block strategically important player moves or try to collect the most points. ", font: .custom(Fonts.mazzardM.rawValue, size: 26), textColor: .mainBrown, borderColor: .mainYellow, borderWidth: 2)
-                    .textCase(.uppercase)
-                    .multilineTextAlignment(.center)
-                    .minimumScaleFactor(0.5)
-                
+//                
+//                
+//                TextWithBorder(text: "The player starts with 6 amulets of different colors, which are located at the bottom of the screen (inventory). The player pulls the selected amulet onto an empty cell. After placing the amulet, it disappears, and a new amulet appears in the inventory.\nOpponent's Actions (AI): The opponent chooses amulets of the same type as the player, but they are marked with a different color. His goal is to block strategically important player moves or try to collect the most points. ", font: .custom(Fonts.mazzardM.rawValue, size: 26), textColor: .mainBrown, borderColor: .mainYellow, borderWidth: 2)
+//                    .textCase(.uppercase)
+//                    .multilineTextAlignment(.center)
+//                    .minimumScaleFactor(0.5)
+//                
                 
                 Spacer()
             }
